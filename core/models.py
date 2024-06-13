a03b956f85597d90e5ac3352bc62d25964ed6ffe
wfarel <wfarel.t@gmail.com>
2024-06-13 11:43:22 -0400

    conflicto reporte_pdf

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ class Producto(models.Model):
     descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0, blank=True, null=True)
+    location = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, proformas_list, proforma_add_client , proforma_new, proforma_edit,\
     agregar_producto_a_detalle, producto_new, eliminar_producto_a_detalle, \
         clientes_list, cliente_new, cliente_edit, cliente_delete, \
-            product_detail, product_edit, productos_list
+            product_detail, product_edit, productos_list, generate_proforma_pdf
 
 urlpatterns = [
     path('', home, name='home'),
@@ -23,4 +23,6 @@ urlpatterns = [
     path('cliente/new/', cliente_new, name='cliente_new'),
     path('cliente/edit/<int:id>/', cliente_edit, name='cliente_edit'),
     path('cliente/delete/<int:id>/', cliente_delete, name='cliente_delete'),
+    #reporte pdf
+    path('proforma/pdf/<int:id>', generate_proforma_pdf, name='generate_proforma_pdf'),
 ]
